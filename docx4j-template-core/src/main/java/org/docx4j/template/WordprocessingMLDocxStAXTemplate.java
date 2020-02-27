691243818388270071a5d76eaa8e610852c77299
wandl <hnxyhcwdl1003@163.com>
2020-02-27 12:33:41 +0800

    新实现

---
+++ docx4j-template-core/src/main/java/org/docx4j/template/WordprocessingMLDocxStAXTemplate.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.docx4j.template;
+
+import java.io.InputStream;
+import java.util.Map;
+
+import org.docx4j.Docx4J;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.template.fonts.FontMapperHolder;
+import org.docx4j.template.handler.VariableReplaceSaTXHandler;
+
+/**
+ * 该模板负责对WordprocessingMLPackage进行普通变量替换和复杂变量替换并返回处理后的WordprocessingMLPackage对象
+ * 备注：该工具只能解决固定模板的word生成（来自：https://blog.csdn.net/qq_35598240/article/details/84439929）
+ * @author <a href="https://github.com/hiwepy">hiwepy</a>
+ */
+public class WordprocessingMLDocxStAXTemplate implements WordprocessingMLTemplate {
+	
+	/**
+	 * 变量占位符开始位，默认：${
+	 */
+	protected String placeholderStart = "${";
+	/**
+	 * 变量占位符结束位，默认：}
+	 */
+	protected String placeholderEnd = "}";
+	
+	/**
+	 * 变量替换方式实现（只能解决固定模板的word生成）
+	 * @param template ：模板内容
+	 * @param variables ：变量
+	 * @return {@link WordprocessingMLPackage} 对象
+	 * @throws Exception ：异常对象
+	 */
+	@Override
+	public WordprocessingMLPackage process(InputStream template, Map<String, Object> variables) throws Exception {
+		// Document loading (required)
+		WordprocessingMLPackage wordMLPackage;
+		if (template == null) {
+			// Create a docx
+			System.out.println("No imput path passed, creating dummy document");
+			wordMLPackage = WordprocessingMLPackage.createPackage();
+			SampleDocument.createContent(wordMLPackage.getMainDocumentPart());	
+		} else {
+			System.out.println("Loading file from InputStream");
+			wordMLPackage = Docx4J.load(template);
+		}
+        if (null != variables && !variables.isEmpty()) {
+        	// 替换变量并输出Word文档 
+        	MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();  
+        	// 替换变量
+        	documentPart.pipe( new VariableReplaceSaTXHandler( this.getPlaceholderStart() , this.getPlaceholderEnd(), variables) );
+         }
+        // 返回WordprocessingMLPackage对象
+		return FontMapperHolder.useFontMapper(wordMLPackage);
+	}
+	
+	public String getPlaceholderStart() {
+		return placeholderStart;
+	}
+
+	public void setPlaceholderStart(String placeholderStart) {
+		this.placeholderStart = placeholderStart;
+	}
+
+	public String getPlaceholderEnd() {
+		return placeholderEnd;
+	}
+
+	public void setPlaceholderEnd(String placeholderEnd) {
+		this.placeholderEnd = placeholderEnd;
+	}
+	
+}
---
+++ docx4j-template-core/src/main/java/org/docx4j/template/handler/VariableReplaceSAXHandler.java
@@ -0,0 +1,69 @@
+/** 
+ * Copyright (C) 2018 Jeebiz (http://jeebiz.net).
+ * All Rights Reserved. 
+ */
+package org.docx4j.template.handler;
+
+import java.util.Map;
+
+import org.docx4j.openpackaging.parts.SAXHandler;
+import org.xml.sax.SAXException;
+
+public class VariableReplaceSAXHandler extends SAXHandler {
+	/**
+	 * 变量占位符开始位，默认：${
+	 */
+	protected String placeholderStart = "${";
+	/**
+	 * 变量占位符结束位，默认：}
+	 */
+	protected String placeholderEnd = "}";
+	protected Map<String, Object> variables;
+
+	public VariableReplaceSAXHandler(Map<String, Object> variables) throws SAXException {
+		super();
+		this.variables = variables;
+	}
+	
+	public VariableReplaceSAXHandler(String placeholderStart, String placeholderEnd ,Map<String, Object> variables) throws SAXException {
+		super();
+		this.placeholderStart = placeholderStart;
+		this.placeholderEnd = placeholderEnd;
+		this.variables = variables;
+	}
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(ch, start, length);
+
+		String wmlString = replace(sb.toString(), 0, new StringBuilder(), variables).toString();
+//		System.out.println(wmlString);
+
+		char[] charOut = wmlString.toCharArray();
+		this.getContentHandler().characters(charOut, 0, charOut.length);
+
+	}
+
+	private StringBuilder replace(String wmlTemplateString, int offset, StringBuilder strB,
+			Map<String, Object> mappings) {
+
+		int startKey = wmlTemplateString.indexOf(placeholderStart, offset);
+		if (startKey == -1)
+			return strB.append(wmlTemplateString.substring(offset));
+		else {
+			strB.append(wmlTemplateString.substring(offset, startKey));
+			int keyEnd = wmlTemplateString.indexOf(placeholderEnd, startKey);
+			String key = wmlTemplateString.substring(startKey + 2, keyEnd);
+			Object val = mappings.get(key);
+			if (val == null) {
+				System.out.println("Invalid key '" + key + "' or key not mapped to a value");
+				strB.append(key);
+			} else {
+				strB.append(val.toString());
+			}
+			return replace(wmlTemplateString, keyEnd + 1, strB, mappings);
+		}
+	}
+}
