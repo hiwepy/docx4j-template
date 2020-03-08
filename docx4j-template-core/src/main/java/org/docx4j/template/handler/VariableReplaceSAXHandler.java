4dbcb13ab64187ed3542360eb5d3d61db83f9afc
wandl <hnxyhcwdl1003@163.com>
2020-03-08 12:06:12 +0800

    增加 Spring Expression Language（SpEL） 支持，实现动态变量解析

--- docx4j-template-core/src/main/java/org/docx4j/template/handler/VariableReplaceSAXHandler.java
+++ docx4j-template-core/src/main/java/org/docx4j/template/handler/VariableReplaceSAXHandler.java
@@ -5,12 +5,20 @@
 package org.docx4j.template.handler;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.docx4j.openpackaging.parts.SAXHandler;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
 public class VariableReplaceSAXHandler extends SAXHandler implements ContentHandler {
+	
+	protected ExpressionParser parser = new SpelExpressionParser();
+	
 	/**
 	 * 变量占位符开始位，默认：${
 	 */
@@ -19,10 +27,26 @@ public class VariableReplaceSAXHandler extends SAXHandler implements ContentHand
 	 * 变量占位符结束位，默认：}
 	 */
 	protected String placeholderEnd = "}";
+	/**
+	 * SPEL表达式占位符开始位，默认：#{
+	 */
+	protected String spelExpressionStart = "#{";
+	/**
+	 * SPEL表达式占位符结束位，默认：}
+	 */
+	protected String spelExpressionEnd = "}";
+	/**
+	 * 变量集合
+	 */
 	protected Map<String, Object> variables;
+	/**
+	 * 表达式上下文对象
+	 */
+	protected EvaluationContext context;
 
 	public VariableReplaceSAXHandler(Map<String, Object> variables) throws SAXException {
 		super();
+		this.initContext();
 	}
 	
 	public VariableReplaceSAXHandler(String placeholderStart, String placeholderEnd ,Map<String, Object> variables) throws SAXException {
@@ -30,6 +54,14 @@ public class VariableReplaceSAXHandler extends SAXHandler implements ContentHand
 		this.placeholderStart = placeholderStart;
 		this.placeholderEnd = placeholderEnd;
 		this.variables = variables;
+		this.initContext();
+	}
+	
+	protected void initContext() {
+		context = new StandardEvaluationContext();  
+        for (Entry<String, Object> entry : variables.entrySet()) {
+        	context.setVariable(entry.getKey(), entry.getValue());
+		}
 	}
 
 	@Override
@@ -51,16 +83,22 @@ public class VariableReplaceSAXHandler extends SAXHandler implements ContentHand
 			Map<String, Object> mappings) {
 
 		int startKey = wmlTemplateString.indexOf(placeholderStart, offset);
-		if (startKey == -1)
+		if (startKey == -1) {
 			return strB.append(wmlTemplateString.substring(offset));
-		else {
+		} else {
 			strB.append(wmlTemplateString.substring(offset, startKey));
 			int keyEnd = wmlTemplateString.indexOf(placeholderEnd, startKey);
 			String key = wmlTemplateString.substring(startKey + 2, keyEnd);
 			Object val = mappings.get(key);
 			if (val == null) {
-				System.out.println("Invalid key '" + key + "' or key not mapped to a value");
-				strB.append(key);
+				String expression = spelExpressionStart + key + spelExpressionEnd;
+				String value = parser.parseExpression(expression).getValue(context, String.class);
+				if(value != null) {
+					strB.append(value);
+				} else {
+					System.out.println("Invalid key '" + key + "' or key not mapped to a value");
+					strB.append(key);
+				}
 			} else {
 				strB.append(val.toString());
 			}
--- docx4j-template-core/src/main/java/org/docx4j/template/handler/VariableReplaceSaTXHandler.java
+++ docx4j-template-core/src/main/java/org/docx4j/template/handler/VariableReplaceSaTXHandler.java
@@ -5,15 +5,23 @@
 package org.docx4j.template.handler;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 
 import org.docx4j.openpackaging.parts.StAXHandlerAbstract;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.xml.sax.SAXException;
 
 public class VariableReplaceSaTXHandler extends StAXHandlerAbstract {
+	
+	protected ExpressionParser parser = new SpelExpressionParser();
+	
 	/**
 	 * 变量占位符开始位，默认：${
 	 */
@@ -22,11 +30,27 @@ public class VariableReplaceSaTXHandler extends StAXHandlerAbstract {
 	 * 变量占位符结束位，默认：}
 	 */
 	protected String placeholderEnd = "}";
+	/**
+	 * SPEL表达式占位符开始位，默认：#{
+	 */
+	protected String spelExpressionStart = "#{";
+	/**
+	 * SPEL表达式占位符结束位，默认：}
+	 */
+	protected String spelExpressionEnd = "}";
+	/**
+	 * 变量集合
+	 */
 	protected Map<String, Object> variables;
-
+	/**
+	 * 表达式上下文对象
+	 */
+	protected EvaluationContext context;
+	
 	public VariableReplaceSaTXHandler(Map<String, Object> variables) throws SAXException {
 		super();
 		this.variables = variables;
+		this.initContext();
 	}
 
 	public VariableReplaceSaTXHandler(String placeholderStart, String placeholderEnd, Map<String, Object> variables)
@@ -35,12 +59,21 @@ public class VariableReplaceSaTXHandler extends StAXHandlerAbstract {
 		this.placeholderStart = placeholderStart;
 		this.placeholderEnd = placeholderEnd;
 		this.variables = variables;
+		this.initContext();
 	}
 
+	protected void initContext() {
+		context = new StandardEvaluationContext();  
+        for (Entry<String, Object> entry : variables.entrySet()) {
+        	context.setVariable(entry.getKey(), entry.getValue());
+		}
+	}
+	
 	@Override
 	public void handleCharacters(XMLStreamReader xmlr, XMLStreamWriter writer) throws XMLStreamException {
 
 		StringBuilder sb = new StringBuilder();
+	
 		sb.append(xmlr.getTextCharacters(), xmlr.getTextStart(), xmlr.getTextLength());
 
 		String wmlString = replace(sb.toString(), 0, new StringBuilder(), variables).toString();
@@ -58,17 +91,23 @@ public class VariableReplaceSaTXHandler extends StAXHandlerAbstract {
 			Map<String, Object> mappings) {
 
 		int startKey = wmlTemplateString.indexOf(placeholderStart, offset);
-		if (startKey == -1)
+		if (startKey == -1) {
 			return strB.append(wmlTemplateString.substring(offset));
-		else {
+		} else {
 			strB.append(wmlTemplateString.substring(offset, startKey));
 			int keyEnd = wmlTemplateString.indexOf(placeholderEnd, startKey);
 			if (keyEnd > 0) {
 				String key = wmlTemplateString.substring(startKey + 2, keyEnd);
 				Object val = mappings.get(key);
 				if (val == null) {
-					System.out.println("Invalid key '" + key + "' or key not mapped to a value");
-					strB.append(key);
+					String expression = spelExpressionStart + key + spelExpressionEnd;
+					String value = parser.parseExpression(expression).getValue(context, String.class);
+					if(value != null) {
+						strB.append(value);
+					} else {
+						System.out.println("Invalid key '" + key + "' or key not mapped to a value");
+						strB.append(key);
+					}
 				} else {
 					strB.append(val.toString());
 				}
